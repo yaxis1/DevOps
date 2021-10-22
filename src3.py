9522f1f63ad37e26d634b359fe66e83fb6f4de23
Ubuntu <ubuntu@ip-172-31-22-155.eu-west-3.compute.internal>
2021-10-22 13:20:20 +0000

    finalising tests

---
+++ src3.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, jsonify
+from flask_restful import Resource, Api
+import requests
+
+
+app = Flask(__name__)
+#api = Api(app)
+
+@app.route('/')
+def main():
+    return render_template('app.html')
+
+@app.route('/send', methods=['POST'])
+def send():
+    if request.method == 'POST':
+        num1 = int(request.form['num1'])
+        num2 = int(request.form['num2'])
+        multiple_num = int(request.form['multiple_num'])
+        text = str(request.form['text'])
+        com_num = int(request.form['com_num'])
+
+        seven_list = []
+        for num in range (num1 , num2 ):
+            if num % multiple_num == 0:
+                seven_list.append(num)
+        if len(seven_list) > com_num:
+            
+        
+            return render_template('app.html', outputDiv = text)
+        else:
+     
+            return render_template('app.html', outputDiv = str(seven_list))
+    
+    else:
+        return render_template('app.html')  
+        
+
+if __name__ == '__main__':
+    app.run(debug=True,port=2021)
+
+
---
+++ test.py
@@ -0,0 +1,27 @@
+from flask import request
+from src3 import app
+import unittest, requests
+
+
+class FlaskTest(unittest.TestCase):
+    url = 'http://localhost:2021/'
+
+    def test_1_home(self):
+        with app.app_context(): 
+            r = requests.get(FlaskTest.url+'/' )
+            self.assertEqual(r.status_code, 200)
+
+    def test_2_send(self):
+        #Check if returns text
+        r = requests.post(FlaskTest.url+'/send' , data = {'num1': 1, 'num2':20, 'multiple_num' : 1, 'text': 'value', 'com_num': 1})
+        return 'value' in r.content.decode('utf-8')
+
+    def test_3_send(self):
+        #Check if returns string of multiples 
+        r = requests.post(FlaskTest.url+'/send' , data = {'num1': 1, 'num2':20, 'multiple_num' : 7, 'text': 'value', 'com_num': 3})
+        return '[7, 14]' in r.content.decode('utf-8')
+         
+ 
+if __name__ == '__main__':
+    unittest.main()
+
