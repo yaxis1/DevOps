cd6af3e693809552b450d7186eff40af3c9c54d3
yaxis1 <vercettisaif@outlook.com>
2021-10-27 00:27:18 +0200

    trials with nginx

--- app/run.py
+++ app/run.py
@@ -25,8 +25,8 @@ def send():
         
 
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=2021)
-    #app.run(debug=True,host='0.0.0.0',port=2021)
+    #from waitress import serve
+    #serve(app, host="0.0.0.0", port=2021)
+    app.run(host='0.0.0.0')
 
 
