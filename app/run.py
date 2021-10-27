168de0c3f45003b2a64979034194591b480d8284
yaxis1 <vercettisaif@outlook.com>
2021-10-27 14:44:09 +0200

    integrating nginx

--- app/run.py
+++ app/run.py
@@ -23,7 +23,6 @@ def send():
     text = str(request.form['text'])
     return render_template('app.html', outputDiv = text)
         
-
 if __name__ == '__main__':
     #from waitress import serve
     #serve(app, host="0.0.0.0", port=2021)
